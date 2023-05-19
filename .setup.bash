e81e40a43b3bb784cf5cd5fc223bee00c3258e23
kinda-raffy <rafat.mahiuddin@gmail.com>
2023-05-20 03:20:38 +1000

    Add tuned orb-slam

--- .setup.bash
+++ .setup.bash
@@ -1,1 +1,8 @@
-source ~/spotters/spot_venv/bin/activate
+alias spot_env='source ~/spotters/spot_venv/bin/activate'
+
+alias wspt='cd ~/spotters/workspace/'
+alias sspt='wspt; source devel/setup.bash'
+alias buildspt='wspt; catkin build; sspt'
+alias cbuildspt='wspt; catkin clean --yes; catkin build; sspt'
+
+alias rcd='roscd'
