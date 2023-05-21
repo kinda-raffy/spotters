2f29ff4c3c5d2fb244d87d658cb0f72e7d825248
kinda-raffy <rafat.mahiuddin@gmail.com>
2023-05-22 04:07:05 +1000

    Initial working server

--- input_server/TrueDepthStreamer/TrueDepthStreamer-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Bridging header for TrueDepthStreamer
-*/
-
-#import "HistogramCalculator.h"
-#import "PointCloudMetalView.h"
