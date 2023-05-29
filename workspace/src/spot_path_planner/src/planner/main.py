a460735b1c1a707406401a0041765e0676c5cbbd
David Hermanto <s3808127@student.rmit.edu.au>
2023-05-29 19:07:30 +0930

    Added pub and sub structure to main

--- workspace/src/spot_path_planner/src/planner/main.py
+++ workspace/src/spot_path_planner/src/planner/main.py
@@ -9,30 +9,40 @@ from geometry_msgs.msg import (
     Quaternion
 )
 
+from navigator import grid, graph, d_star_lite
+from nav_msgs.msg import OccupancyGrid
+
 from spot_driver.spot_ros import SpotROS
 
-def publish_message(pub, message, rate):
+def occupancy_callback(data):
+    print("Received occupancy grid")
+
+def goal_callback(data):
+    print("Received new goal")
+
+def publish_path(pub_path, message, rate):
     while not rospy.is_shutdown():
-        pub.publish(message)
+        pub_path.publish(message)
         rate.sleep()
 
-def publish_next_target():
-    pub = rospy.Publisher('/spot_path', Point, queue_size=10)
-    rate = rospy.Rate(10)
-    message = Point(
-        x=0,
-        y=0,
-        z=0
-    )
-    publish_message(pub, message, rate)
-    pub.publish(Twist())
+# Should return the path needed to publish
+def calc_path():
+    print("Calculating Path")
+    
 
 def main():
     SR = SpotROS()
     SR.main()
 
-    rospy.init_node('publisher_node', anonymous=True)
-    publish_next_target()
+    rospy.init_node('path_planner', anonymous=True)
+    
+    sub_goal = rospy.Subscriber('/goal', Pose, occupancy_callback)
+    sub_grid = rospy.Subscriber('/grid', OccupancyGrid, occupancy_callback)
+    pub_path = rospy.Publisher('/spot_path', Pose, queue_size=10)
+
+    rate = rospy.Rate(10)
+
+    calc_path()
 
 
 if __name__ == '__main__':
