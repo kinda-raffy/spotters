e47675bbd4b90895e4b6a5ef39898e63a14d195c
kinda_raffy <rafat.mahiuddin@gmail.com>
2023-06-12 03:18:07 +1000

    fix: adjust refresh params

--- workspace/src/spot_cartographer/src/spot_cartographer/sync.py
+++ workspace/src/spot_cartographer/src/spot_cartographer/sync.py
@@ -25,14 +25,14 @@ class PointCloudChangeTracker:
         self.growth_aggregate = 0
         self.update_count = 0
         self.avg_point_count_increase = 0
-        self.change_factor = 2.0
+        self.change_factor = 1.5
 
     def detect_significant_changes(self, cloud: PointCloud2) -> None:
         cloud_point_count = cloud.row_step * cloud.height
         change_in_points = cloud_point_count - self.last_point_count
         change_threshold = round(self.avg_point_count_increase * self.change_factor)
         void = Empty()
-        if change_in_points > change_threshold:
+        if change_in_points < -change_threshold:
             rospy.logdebug(f"detected map merge with change of size: {change_in_points}")
             self.squealer.publish(void)
             self.update_count = 0
@@ -43,3 +43,4 @@ class PointCloudChangeTracker:
         self.growth_aggregate += change_in_points
         self.update_count += 1
         self.avg_point_count_increase = round(self.growth_aggregate / self.update_count)
+        self.last_point_count = cloud_point_count
