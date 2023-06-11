cde75bba7ace821eb6f489b1b7c00a290e4567a7
kinda_raffy <rafat.mahiuddin@gmail.com>
2023-06-12 03:26:36 +1000

    fix: adjust refresh params

--- workspace/src/spot_cartographer/src/spot_cartographer/sync.py
+++ workspace/src/spot_cartographer/src/spot_cartographer/sync.py
@@ -1,4 +1,5 @@
 import rospy
+import time
 from std_msgs.msg import Empty
 from sensor_msgs.msg import PointCloud2
 
@@ -20,6 +21,9 @@ class PointCloudChangeTracker:
             Empty,
             queue_size=1,
         )
+
+        self.time_interval_sec = 2
+        self.last_reset_time = time.time()
         self.based_loss_threshold = 1000
         self.last_point_count = 0
         self.growth_aggregate = 0
@@ -28,6 +32,9 @@ class PointCloudChangeTracker:
         self.change_factor = 1.5
 
     def detect_significant_changes(self, cloud: PointCloud2) -> None:
+        now = time.time()
+        if now < self.last_reset_time + self.time_interval_sec:
+            return
         cloud_point_count = cloud.row_step * cloud.height
         change_in_points = cloud_point_count - self.last_point_count
         change_threshold = round(self.avg_point_count_increase * self.change_factor)
@@ -37,9 +44,11 @@ class PointCloudChangeTracker:
             self.squealer.publish(void)
             self.update_count = 0
             self.growth_aggregate = 0
+            self.last_reset_time = now
         elif change_in_points < -self.based_loss_threshold:
             rospy.logdebug(f"detected new_map with loss of {change_in_points}")
             self.shrinker.publish(void)
+            self.last_reset_time = now
         self.growth_aggregate += change_in_points
         self.update_count += 1
         self.avg_point_count_increase = round(self.growth_aggregate / self.update_count)
